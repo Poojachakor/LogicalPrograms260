a2a4fb353252a1ded3c40bfafb5a21ec60b02487
Poojachakor <chakorpooja07@gmail.com>
2023-02-23 17:01:58 +0530

    Add: UC3 file

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void PrimeNumbers()
+        {
+            int n;
+            Console.WriteLine("Enter the Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            PerfectNumber.perfectNo();
+            PrimeNumber.PrimeNumbers();
 
         }
     }
