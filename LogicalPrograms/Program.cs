73f1f7d1fa55dd0770fe67026c4218fd3f6d24fe
Poojachakor <chakorpooja07@gmail.com>
2023-02-24 22:03:45 +0530

    Add: UC6 file

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,10 +6,8 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            CouponNumbers.CouponGenerator();
-        
-
-
+            SimulateStopwatchProgram.StopWatchSimulation();
+            
 
         }
     }
